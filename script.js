e532c6fc70cceb671472c991960c2346130523ec
Valeria Cartagena <valeriacartagena@DN0a1fc205.SUNet>
2024-02-24 17:43:04 -0800

    commit

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ const tips = [
     "Another book suggestion: Outliers.",
     "Another book suggestion: Through the Glass Ceiling to the Stars.",
     "Huang says don't learn how to code.",
-    ""Trust me, I'm lying.""
+    ""Trust me, I'm lying."",
+    "Infinito positivo no contable elevado al ..."
     
     // Add more tips as needed
 ];
